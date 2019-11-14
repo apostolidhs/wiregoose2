0e34d35c083790a04f0d4aa1f10feef7e5f8b38e
apostolidhs <john.apostolidh@gmail.com>
2019-11-15 00:12:28 +0200

    Initiated frontend app.

---
+++ src/app.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import {Grommet} from 'grommet';
+
+const theme = {
+  global: {
+    font: {
+      family: 'Roboto',
+      size: '18px',
+      height: '20px'
+    }
+  }
+};
+
+function App() {
+  return (
+    <Grommet theme={theme}>
+      <header className="App-header">
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+          Learn React
+        </a>
+      </header>
+    </Grommet>
+  );
+}
+
+export default App;
